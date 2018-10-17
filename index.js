07070ea371ffb4d7340ef148b462e0658b4e471b
Chris Tanner <me@itschr.is>
2018-10-16 23:12:25 -0700

    fix eslint errors

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-
+'use strict';
 
 const hook = require('./src/hook.js');
 const fastify = require('fastify')({
-  logger: true
-})
+  logger: true,
+});
 
-fastify.post('/hook', hook)
+fastify.post('/hook', hook);
 
 fastify.listen(80, (err, address) => {
-  if (err) throw err
-  fastify.log.info(`server listening on ${address}`)
-})
+  if (err) throw err;
+  fastify.log.info(`server listening on ${address}`);
+});
